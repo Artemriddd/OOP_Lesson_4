86223bec8288af7dc536c5e8d17d8467648c9f2a
Artem <Artemrid@yandex.ru>
2022-07-08 19:27:35 +0300

    Создана библиотека классов. С помощью ILDASM получится открыть только DLL от exe. Если я правильно понял(нагуглил), связано с тем, что .net6 не использует IL-код.

--- BuildCreator/Creator.cs
+++ BuildCreator/Creator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Less_4
 {
-    class Creator
+    public class Creator
     {
         private Creator()
         {
--- Less_4/Program.cs
+++ Less_4/Program.cs
@@ -9,45 +9,6 @@ namespace Less_4
         {
             Hashtable hashtable = new Hashtable(); // Хэш таблица
 
-            Building building = new Building(); // Заполнение через методы
-            Building.IdCreator();
-            building.HeightSetter(100);
-            building.FloorsSetter(5);
-            building.EntranceSetter(5);
-            building.FlatsSetter(50);
-            Console.WriteLine($"Номер здания: {Building.IdGetter()}");
-            Console.WriteLine($"Высота здания: {building.HeightGetter()}");
-            Console.WriteLine($"Количество этажей: {building.FloorsGetter()}");
-            Console.WriteLine($"Количество квартир: {building.FlatsGetter()}");
-            Console.WriteLine($"Количество подъездов: {building.EntranceGetter()}");
-
-            // Проверка методов по вычислению высоты этажа, кол-ва квартир в подъезде и кол-ва квартир на этаже
-            Console.WriteLine($"Высота этажа: {building.FloorHeingt()}");
-            Console.WriteLine($"Квартир в подъезде: {building.FlatsInEntrance()}");
-            Console.WriteLine($"Квартир на этаже: {building.FlatsInFloor()}");
-
-            hashtable.Add(building.Id, building); //Добавление здания в хэш таблицу
-
-            Building building2 = new Building(); // Заполнение через свойства
-            Building.IdCreator();
-            building2.Height = 200;
-            building2.Floors = 10;
-            building2.Entrance = 5;
-            building2.Flats = 50;
-            Console.WriteLine($"Номер здания: {Building.IdGetter()}");
-            Console.WriteLine($"Высота здания: {building2.Height}");
-            Console.WriteLine($"Количество этажей: {building2.Floors}");
-            Console.WriteLine($"Количество квартир: {building2.Flats}");
-            Console.WriteLine($"Количество подъездов: {building2.Entrance}");
-
-
-            // Проверка методов по вычислению высоты этажа, кол-ва квартир в подъезде и кол-ва квартир на этаже
-            Console.WriteLine($"Высота этажа: {building2.FloorHeingt()}");
-            Console.WriteLine($"Квартир в подъезде: {building2.FlatsInEntrance()}");
-            Console.WriteLine($"Квартир на этаже: {building2.FlatsInFloor()}");
-
-            hashtable.Add(building2.Id, building2); //Добавление здания в хэш таблицу
-
             Building building3 = Creator.CreateBuild(200, 10, 50, 5); // Заполнение через фабричный метод Creator
 
             // Проверка методов по вычислению высоты этажа, кол-ва квартир в подъезде и кол-ва квартир на этаже
@@ -67,12 +28,6 @@ namespace Less_4
             hashtable.Add(building4.Id, building4);  //Добавление здания в хэш таблицу
 
             foreach (DictionaryEntry elem in hashtable) // Провека хэш-таблицы
-            {
-                Console.WriteLine($"Ключ {elem.Key} и значение {elem.Value}");
-            }
-            hashtable.Remove(1); // Удаление по ключу "Уникальный номер здания" элемента из хэш-таблицы
-
-            foreach (DictionaryEntry elem in hashtable) // Проверка что здание удалилось из хэш-таблицы
             {
                 Console.WriteLine($"Ключ {elem.Key} и значение {elem.Value}");
             }
